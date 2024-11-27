a1c5b39794b768307054dd77058ea6c59c8db7cc
Priyansh2604 <ppriyansh790@gmail.com>
2024-11-27 23:04:43 +0530

    Add files via upload
    I

---
+++ Rock paper scissor.js
@@ -0,0 +1,71 @@
+let Choise=["Rock","paper","Scissor"];
+$("button").click(function game() {
+  
+
+
+let ComputerChoise=Choise[Math.floor(Math.random()*Choise.length)];
+console.log(ComputerChoise)
+$("#Rock").click(function () {
+  console.log("Rock")
+  if (ComputerChoise==="Rock") {
+    console.log("Draw")
+    $("h3").text("Draw")
+    
+  }
+  if (ComputerChoise==="Scissor") {
+    console.log("User Wins")
+    $("h3").text("Rock beats Scissor User Wins")
+   
+
+  }
+  if (ComputerChoise==="paper") {
+    console.log("Computer Wins")
+    $("h3").text("Paper beats Rock Computer Wins")
+   
+  }
+})
+$("#paper").click(function () {
+  console.log("paper")
+  if (ComputerChoise==="paper") {
+    console.log("Draw")
+    $("h3").text("Draw")
+   
+  }
+  if (ComputerChoise==="Scissor") {
+    console.log("Computer Wins")
+    $("h3").text("Scissor beats paper Computer Wins")
+   
+  }
+  if (ComputerChoise==="Rock") {
+    console.log("User Wins")
+    $("h3").text("Paper beats Rock User Wins")
+   
+  }
+  
+})
+$("#Scissor").click(function () {
+  console.log("Scissor")
+  if (ComputerChoise==="Scissor") {
+    console.log("Draw")
+    $("h3").text("Draw")
+    
+  }
+  if (ComputerChoise==="paper") {
+    console.log("User Wins")
+    $("h3").text("Scissor beats Paper User Wins")
+    
+  }
+  if (ComputerChoise==="Rock") {
+    console.log("Computer Wins")
+    $("h3").text("Rock beats Scissor Computer Wins")
+    
+    
+  }
+  $(".playAgain").click(location.reload,
+    
+  )
+  
+})
+})
+
+
